d53157fe26d56fb8e3a0a4095a0a9bd905588978
oscarstorm <oscar.emil.storm@gmail.com>
2023-09-29 22:48:04 +0200

    style update, and week1

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-
 import express from "express";
+import path from "path";
 const app = express();
 app.use(express.static("public"));
-import path from "path"
 
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("public/home.html"));
+});
 
-app.get("/", (req, res) =>{
-    res.sendFile(path.resolve("public/home.html"));
-})
+app.get("/week1", (req, res) => {
+  res.sendFile(path.resolve("public/week1.html"));
+});
 
-app.get("/week1", (req, res) =>{
-    res.sendFile(path.resolve("public/week1.html"));
-})
+app.get("/week2", (req, res) => {
+  res.sendFile(path.resolve("public/week2.html"));
+});
 
-app.get("/week2", (req, res) =>{
-    res.sendFile(path.resolve("public/week2.html"));
-})
+app.get("/week3", (req, res) => {
+  res.sendFile(path.resolve("public/week3.html"));
+});
+app.get("/week4", (req, res) => {
+  res.sendFile(path.resolve("public/week4.html"));
+});
+app.get("/week5", (req, res) => {
+  res.sendFile(path.resolve("public/week5.html"));
+});
 
-app.get("/week3", (req, res) =>{
-    res.sendFile(path.resolve("public/week3.html"));
-})
-app.get("/week4", (req, res) =>{
-    res.sendFile(path.resolve("public/week4.html"));
-})
-app.get("/week5", (req, res) =>{
-    res.sendFile(path.resolve("public/week5.html"));
-})
-
-
-const PORT = (8080);
-app.listen(PORT)
-
-
+const PORT = 8080;
+app.listen(PORT);
