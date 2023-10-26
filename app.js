8a9be6669f170454f688b60e748bb8c57993da1b
oscarstorm <oscar.emil.storm@gmail.com>
2023-10-26 14:44:01 +0200

    SSR implemented, and week5

--- app.js
+++ app.js
@@ -4,26 +4,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  // Dummy check for username and password
-  if(username === 'oscar' && password === 'storm') {
-    return res.redirect('/home.html');
-  }
-  
-  return res.status(401).send('Invalid login credentials');
-});
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+// Set the views directory
+app.set("views", "./views");
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("public/login.html"));
 });
 
-app.get("/home", (req, res) =>{
+app.get("/home", (req, res) => {
   res.sendFile(path.resolve("public/home.html"));
-})
+});
 
 app.get("/week1", (req, res) => {
   res.sendFile(path.resolve("public/week1.html"));
@@ -36,11 +28,24 @@ app.get("/week2", (req, res) => {
 app.get("/week3", (req, res) => {
   res.sendFile(path.resolve("public/week3.html"));
 });
+
 app.get("/week4", (req, res) => {
   res.sendFile(path.resolve("public/week4.html"));
 });
+
+//SSR using ejs
 app.get("/week5", (req, res) => {
-  res.sendFile(path.resolve("public/week5.html"));
+  res.render(path.resolve("views/week5.ejs"));
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Dummy check for username and password
+  if (username === "oscar" && password === "storm") {
+    return res.redirect("/home.html");
+  }
+  return res.status(401).send("Invalid login credentials");
 });
 
 const PORT = 8080;
