16b03cc448e4cccefb394083928acbdcc9b57251
OscarStorm <oscar.emil.storm@gmail.com>
2025-01-25 20:30:20 +0100

    this is it

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url"; // Import fileURLToPath for path conversion
+
 const app = express();
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public"))); // Make sure the path points to 'public'
+app.use(express.static(path.join(__dirname, "public"))); // Ensure correct path to 'public'
 
-// Set EJS as the view engine
 app.set("view engine", "ejs");
-// Set the views directory
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("public/login.html"));
+  res.sendFile(path.resolve(__dirname, "public/login.html"));
 });
 
 app.get("/home", (req, res) => {
-  res.sendFile(path.resolve("public/home.html"));
+  res.sendFile(path.resolve(__dirname, "public/home.html"));
 });
 
 app.get("/week1", (req, res) => {
-  res.sendFile(path.resolve("public/week1.html"));
+  res.sendFile(path.resolve(__dirname, "public/week1.html"));
 });
 
 app.get("/week2", (req, res) => {
-  res.sendFile(path.resolve("public/week2.html"));
+  res.sendFile(path.resolve(__dirname, "public/week2.html"));
 });
 
 app.get("/week3", (req, res) => {
-  res.sendFile(path.resolve("public/week3.html"));
+  res.sendFile(path.resolve(__dirname, "public/week3.html"));
 });
 
 app.get("/week4", (req, res) => {
-  res.sendFile(path.resolve("public/week4.html"));
+  res.sendFile(path.resolve(__dirname, "public/week4.html"));
 });
 
-//SSR using ejs
+// SSR using ejs
 app.get("/week5", (req, res) => {
-  res.render(path.resolve("views/week5.ejs"));
+  res.render(path.resolve(__dirname, "views/week5.ejs"));
 });
 
 app.post("/login", (req, res) => {
@@ -50,6 +55,8 @@ app.post("/login", (req, res) => {
 });
 
 const PORT = 8080;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
